718cf09097e2a5c3f3457ae54a34f989bc811eac
Shpend Mula <shmula@hotmail.com>
2021-04-30 13:43:45 +0200

    updated javascript

--- js/script.js
+++ js/script.js
@@ -1,14 +1,29 @@
-const fileSelector = document.querySelector('.file');
+const fileSelector = document.querySelector('.upload-file-input');
 let jsonTextarea = document.querySelector('.json');
 let csvTextarea = document.querySelector('.csv');
 const convertBtn = document.querySelector('.convert');
-
+const clearBtn = document.querySelector('.clear');
+const form = document.querySelector('form');
+const downloadBtn = document.querySelector(".download");
 
 
 convertBtn.addEventListener('click', (ev) => {
     ev.preventDefault();
     let jsonString = jsonTextarea.value;
-    let arr = isValidJSONString(jsonString) ? JSON.parse(jsonString) : alertUser();
+    
+    if(jsonString === "") {
+        alertUser('fail','Input is NOT a valid JSON');
+        return;
+    }
+
+    let arr;
+    if(!isValidJSONString(jsonString)) {
+        alertUser('fail','Input is NOT a valid JSON');
+        return;
+    }  
+
+    alertUser('success','Input is a valid JSON')
+    arr = JSON.parse(jsonString);
     let headers = [];
     let data = [];
     let key = Object.keys(arr[0]);
@@ -17,6 +32,7 @@ convertBtn.addEventListener('click', (ev) => {
         let value = Object.values(obj);
         data.push(value)
     })
+    console.log(headers,data)
     headers = headers.join("");
     data = data.join("\n");
 
@@ -28,10 +44,12 @@ fileSelector.addEventListener('change', (ev) => {
     // returns a FileList Array like Object not array. Numbered properties that point to objects which are files with properties
     reader.onload = () => {
         let JSONFile = reader.result;
-        let arr = isValidJSONString(JSONFile) ? JSON.parse(JSONFile) : alertUser();
-        if(arr.length > 0) {
-            console.log('hello')
+        
+        if(isValidJSONString(JSONFile)) {
             jsonTextarea.value = JSONFile;
+        } else {
+            jsonTextarea.value = JSONFile;
+            alertUser('fail','Input is NOT a valid JSON');
         }
     }
     
@@ -40,17 +58,59 @@ fileSelector.addEventListener('change', (ev) => {
     
 })
 
-jsonTextarea.addEventListener('input',()=> {
-    let jsonString = jsonTextarea.value;
-    let arr = isValidJSONString(jsonString) ? JSON.parse(jsonString) : alertUser();
-    if(arr.length > 0) {
-        
-        
-    }
+clearBtn.addEventListener('click', () => {
+    form.reset();
+    
 })
 
-function alertUser() {
-    return []; //If not JSON file
+downloadBtn.addEventListener('click',(e) => {
+    e.preventDefault();
+    e.stopPropagation()
+    let text = csvTextarea.value;
+    let blob = new Blob([text], {type: "text/csv;charset=utf-8"});
+    let blobUrl = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+
+    // Set link's href to point to the Blob URL
+    link.href = blobUrl;
+    link.download = "csvfile";
+
+    // Append link to the body
+    document.body.appendChild(link);
+
+    // Dispatch click event on the link
+    // This is necessary as link.click() does not work on the latest firefox
+    link.dispatchEvent(
+        new MouseEvent('click', { 
+        bubbles: true, 
+        cancelable: true, 
+        view: window 
+        })
+    );
+
+    // Remove link from body
+    document.body.removeChild(link);
+})
+
+ 
+function alertUser(className,message) {
+    let alert = document.querySelector('.alert');
+    if(className === 'fail') {
+        alert.classList.add(className);
+        alert.textContent = message;
+        setTimeout(() =>{
+            alert.classList.remove(className);
+        },2000)
+        
+        
+    } else {
+        alert.classList.add(className);
+        alert.textContent = message;
+        setTimeout(() =>{
+            alert.classList.remove(className);
+        },2000)
+        
+    }
 }
 
 function isValidJSONString(str) {
